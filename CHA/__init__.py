6dd6479551141d95f112b76f7d1c3708f52efabb
RoyNisimov <roynisimov@gmail.com>
2023-11-03 20:40:05 +0200

    Ver 0.4.4

--- CHA/__init__.py
+++ CHA/__init__.py
@@ -2,6 +2,3 @@ __all__ = ['Feistel64', 'CHAObject', 'HashMaker', 'CHAF', 'Hashing_Algorithms']
 from .CHAF import Feistel64
 from .Hashing_Algorithms import CHAObject
 from .Hashing_Algorithms import HashMaker
-from CHAF import Feistel64
-from Hashing_Algorithms import CHAObject
-from Hashing_Algorithms import HashMaker
