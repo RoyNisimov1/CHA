f303118e20295ba614ebe1b99c88c49b30d0e5a9
RoyNisimov <roynisimov@gmail.com>
2023-12-02 13:18:53 +0200

    Fixed a bug

--- CHA/BlackFrog.py
+++ CHA/BlackFrog.py
@@ -83,7 +83,6 @@ class BlackFrogKey:
     @staticmethod
     def load(data, passcode=b''):
         data = PEM.import_PEM(data, passcode)
-        print(data)
         d: dict = json.loads(data)
         de = {key: BlackFrogKey.bti(b64decode(d[key])) for key in d.keys()}
         if de['D'] == 0: de['D'] = None
